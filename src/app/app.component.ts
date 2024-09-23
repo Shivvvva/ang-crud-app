dd91dd8657c061a7ce17a31d3d97a8aadcdd070a
Shiva <macbookpro@Shivas-MacBook-Pro.local>
2024-09-23 19:01:17 +0530

    added new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EditComponent } from './edit/edit.component';
+import { CreateComponent } from './create/create.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    EditComponent,
+    CreateComponent,
+    NotFoundComponent,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'crud-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'edit', component: EditComponent },
+  { path: '**', component: NotFoundComponent },
+];
